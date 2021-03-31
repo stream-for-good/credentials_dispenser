05568f3150a3f7b6f11722aff639cebff8e129c9
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2021-04-01 00:56:11 +0200

    ifixed requirements

--- app/app/main.py
+++ app/app/main.py
@@ -2,7 +2,6 @@ import sys
 import os
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask
-from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -21,7 +20,6 @@ app.config.from_object(config)
 cache = Cache(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-toolbar = DebugToolbarExtension(app)
 ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 from app.routes import *
 
